1fbc6384b8b9878694247c94987736c558ef4f72
Anatoliy-Mostovoy <75166498+Anatoliy-Mostovoy@users.noreply.github.com>
2021-06-17 15:14:54 +0000

    Deploying to gh-pages from @ Anatoliy-Mostovoy/team-proj-demo@051c355da54b109a59035a1031b4077b9a3f45af 🚀

--- src.2a669723.js
+++ src.2a669723.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),console.log("Hello World"),console.log("test push from Ivan (test2)");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-proj-demo/src.e17eea11.js.map
+//# sourceMappingURL=/team-proj-demo/src.2a669723.js.map
