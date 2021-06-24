0bf5bde405c33b9da8820a07e362b0412e670dd5
Anatoliy-Mostovoy <75166498+Anatoliy-Mostovoy@users.noreply.github.com>
2021-06-24 20:17:57 +0000

    Deploying to gh-pages from @ Anatoliy-Mostovoy/team-proj-demo@7338124437641822516814c36c5d8eb8dc45f865 🚀

--- src.528857c1.js
+++ src.528857c1.js
@@ -1,6 +1,6 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");try{console.log("до ошибки"),myVar,console.log("после ошибки")}catch(o){console.dir(o.stack),console.log("ОШИБКА!!")}console.log("после трай");
+"use strict";function e(e){return Promise.resolve({strawberry:1,kiwi:2,apple:3}[e])}async function s(){await e("apple")}function n(){e("apple").then(s=>{console.log(s),e("kiwi").then(e=>console.log(e))})}require("./sass/main.scss"),n();
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-proj-demo/src.06f52c57.js.map
+//# sourceMappingURL=/team-proj-demo/src.528857c1.js.map
